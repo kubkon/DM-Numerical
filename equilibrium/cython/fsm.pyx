201b0eb5f0e97ba5dc990a6262e25f575907e328
Jakub Konka <kubkon@gmail.com>
2013-07-03 14:20:08 +0100

    Create pure C implementation of the ODE system.

--- equilibrium/cython/fsm.pyx
+++ equilibrium/cython/fsm.pyx
@@ -1,33 +1,32 @@
 from cython_gsl cimport *
 from libc.stdlib cimport calloc, free
-cimport numpy as cnp
 import numpy as np
-
-cnp.import_array()
 
 ctypedef struct Tode:
   int n
   double * params
-  int f(int, double *, double, double *, double *)
+  int f(int, double *, double, double *, double *) nogil
 
-cdef int f(int n, double * params, double t, double * y, double * f) with gil:
-  cdef cnp.npy_intp shape[1]
-  shape[0] = <cnp.npy_intp> n
-  ys = cnp.PyArray_SimpleNewFromData(1, shape, cnp.NPY_DOUBLE, y)
-  uppers = cnp.PyArray_SimpleNewFromData(1, shape, cnp.NPY_DOUBLE, params)
-  rs = np.empty(n, dtype=np.float64)
+cdef int f(int n, double * params, double t, double * y, double * f) nogil:
+  cdef double * rs = <double *> calloc(n, sizeof(double))
+  cdef int i
+  cdef double r, rs_sum
+  rs_sum = 0
+
   for i from 0 <= i < n:
-    rs[i] = 1.0 / (t - ys[i])
-  const = np.ones(n) * (np.sum(rs) / (n - 1))
-  fs = (uppers - ys) * (const - rs)
+    r = 1 / (t - y[i])
+    rs[i] = r
+    rs_sum += r
+
   for i from 0 <= i < n:
-    f[i] = fs[i]
+    f[i] = (params[i] - y[i]) * (rs_sum / (n-1) - rs[i])
+
+  free(rs)
   return GSL_SUCCESS
 
 cdef int ode(double t, double y[], double f[], void *params) nogil:
   cdef Tode * P = <Tode *> params
-  with gil:
-    P.f(P.n, P.params, t, y, f)
+  P.f(P.n, P.params, t, y, f)
   return GSL_SUCCESS
 
 def solve(initial, uppers, bids):
@@ -49,11 +48,11 @@ def solve(initial, uppers, bids):
 
   cdef double hstart, epsAbs
   hstart = (bids[1] - bids[0]) / 100.0
-  epsAbs = 1.49012e-8
+  epsAbs = epsRel = 1.49012e-8
   cdef gsl_odeiv2_driver * d
   d = gsl_odeiv2_driver_alloc_y_new(
       &sys, gsl_odeiv2_step_rkf45,
-      hstart, epsAbs, 0.0)
+      hstart, epsAbs, epsRel)
 
   cdef double *y = <double *> calloc(n, sizeof(double))
   if y is NULL:
