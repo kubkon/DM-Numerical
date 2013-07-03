e83f2fb459dac92dc659404f42b0d68d92c2d677
Jakub Konka <kubkon@gmail.com>
2013-07-03 16:35:16 +0100

    Reorganize folder structure.

--- equilibrium/cython/fsm/fsm_main.py
+++ equilibrium/cython/fsm/fsm_main.py
@@ -13,7 +13,7 @@ def upper_bound_bids(lowers, uppers):
   # tabulate the range of permissible values
   vals = np.linspace(uppers[0], uppers[1], 10000)
   tabulated = []
-  # solve the optimization problem in Eq. (1.14) in the thesis
+  # solve the optimization problem in Eq. (1.8) in the thesis
   for v in vals:
     probs = [1-uniform(loc=l, scale=(u-l)).cdf(v) for l, u in zip(lowers[1:], uppers[1:])]
     tabulated += [(v - uppers[0]) * reduce(lambda p,r: p*r, probs, 1)]
