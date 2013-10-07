e987518a97d71563e8ad48da04fc1b48316a532d
Jakub Konka <kubkon@gmail.com>
2013-10-07 11:09:46 +0100

    Use NumPy arrays in FSM algorithm.

--- equilibrium/cython/fsm/main.py
+++ equilibrium/cython/fsm/main.py
@@ -8,10 +8,14 @@ from .fsm import solve
 
 # set the scenario
 w = 0.85
-reputations = [0.2, 0.4, 0.6, 0.8]
+reputations = np.array([0.2, 0.4, 0.6, 0.8], dtype=np.float)
+n = reputations.size
 # compute an array of lower and upper extremities
-lowers = np.array([(1-w)*r for r in reputations])
-uppers = np.array([(1-w)*r + w for r in reputations])
+lowers = np.empty(n, dtype=np.float)
+uppers = np.empty(n, dtype=np.float)
+for i in np.arange(n):
+  lowers[i] = (1-w) * reputations[i]
+  uppers[i] = (1-w) * reputations[i] + w
 # estimate the upper bound on bids
 b_upper = upper_bound_bids(lowers, uppers)
 
@@ -19,25 +23,30 @@ b_upper = upper_bound_bids(lowers, uppers)
 low = lowers[1]
 high = b_upper
 epsilon = 1e-6
+num = 10000
+cond1 = np.empty(num, dtype=np.bool)
+cond2 = np.empty(num, dtype=np.bool)
+cond3 = np.empty(num-1, dtype=np.bool)
 
 # run the FSM algorithm until the estimate of the lower bound
 # on bids is found
 while high - low > epsilon:
   guess = 0.5 * (low + high)
-  bids = np.linspace(guess, b_upper, num=10000, endpoint=False)
+  bids = np.linspace(guess, b_upper, num=num, endpoint=False)
   try:
     costs = solve(lowers, uppers, bids).T
   except Exception:
     # if an error is raised, set low to guess and continue
     low = guess
     continue
-  cond1, cond2 = [], []
-  for l,c in zip(lowers, costs):
-    for x,b in zip(c, bids):
-        cond1 += [l <= x and x <= b_upper]
-        cond2 += [b > x]
-  cond3 = [b1 < b2 for b1, b2 in zip(bids, bids[1:])]
-  if all(cond1 + cond2 + cond3):
+  for i in np.arange(n):
+    for j in np.arange(num):
+      x = costs[i][j]
+      cond1[j] = lowers[i] <= x and x <= b_upper
+      cond2[j] = bids[j] > x
+  for i in np.arange(1, num):
+    cond3[i-1] = bids[i-1] < bids[i]
+  if np.all(cond1) and np.all(cond2) and np.all(cond3):
     high = guess
   else:
     low = guess
@@ -46,7 +55,7 @@ print("Estimated lower bound on bids: %r" % guess)
 
 # save the results in a file
 with open('fsm.out', 'wt') as f:
-  labels = ['w', 'reps', 'bids'] + ['costs_{}'.format(i) for i in range(len(reputations))]
+  labels = ['w', 'reps', 'bids'] + ['costs_{}'.format(i) for i in range(n)]
   labels = ' '.join(labels)
   values = [w, reputations, bids.tolist()] + [c.tolist() for c in costs]
   values = ' '.join(map(lambda x: repr(x).replace(' ', ''), values))
