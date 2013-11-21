4d8a8a901784a018743d3f4b4672f3a2b0a9d62b
Jakub Konka <kubkon@gmail.com>
2013-11-21 19:16:44 +0000

    Compare auctions based on expected utilities accrued.

--- equilibrium/cython/compare.py
+++ equilibrium/cython/compare.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc
+import scipy.stats as ss
 
 import bajari.fsm.main as bajari
 import dm.fsm.main as dm
@@ -12,8 +13,27 @@ rc('text', usetex=True)
 matplotlib.rcParams.update({'font.size': 14, 'legend.fontsize': 14})
 
 
+def estimate_winning_probs(costs, func, params):
+    # initialize
+    shape = costs.shape
+    probs = np.empty(shape, np.float)
+    n, m = shape[0], shape[1]
+
+    # compute probabilities
+    for i in np.arange(n):
+        # get index of the competing bidder
+        j = (i+1) % 2
+        # extract cdf function
+        prob_func = func(**params[j])
+
+        for k in np.arange(m):
+            probs[i][k] = 1 - prob_func.cdf(costs[i][k])
+
+    return probs
+
+
 # set the scenario
-w = 0.5
+w = 0.7
 reputations = np.array([0.25, 0.75], dtype=np.float)
 n = reputations.size
 
@@ -33,53 +53,87 @@ for i in np.arange(n):
     mu = lowers[i] + w / 2
     sigma = w / 4
     params.append({'mu': mu, 'sigma': sigma})
-print(params)
-print(uppers)
 
 # compute approximations
 dm_bids, dm_costs = dm.solve(w, reputations)
 bajari_bids, bajari_costs = bajari.solve(support, params)
 
-# chuck away bids & costs that are irrelevant
-t_bajari_costs, t_bajari_bids = [], []
+# compute probabilities of winning for both auctions
+# 1. DM
+dm_params = [{'loc': lowers[i], 'shape': w} for i in np.arange(n)]
+dm_probs = estimate_winning_probs(dm_costs, ss.uniform, dm_params)
+
+# 2. Bajari
+bajari_params = [{'loc': p['mu'], 'shape': p['sigma']} for p in params]
+bajari_probs = estimate_winning_probs(bajari_costs, ss.norm, bajari_params)
+
+# compute expected utilities
+dm_exp_utilities = np.empty(dm_costs.shape, np.float)
+bajari_exp_utilities = np.empty(bajari_costs.shape, np.float)
 
 for i in np.arange(n):
+    dm_exp_utilities[i] = np.multiply((dm_bids - dm_costs[i]), dm_probs[i])
+    bajari_exp_utilities[i] = np.multiply((bajari_bids - bajari_costs[i]), bajari_probs[i])
+
+# chuck away costs & utilities that are irrelevant
+t_bajari_costs, t_bajari_exps = [], []
+
+for i in np.arange(n):
+    exps = bajari_exp_utilities[i]
     costs = bajari_costs[i]
     low, high = dm_costs[i][0], dm_costs[i][-1]
-    t_bids = []
+    t_exps = []
     t_costs = []
 
     for j in np.arange(costs.size):
         if costs[j] < low or costs[j] > high:
             continue
 
-        t_bids.append(bajari_bids[j])
+        t_exps.append(exps[j])
         t_costs.append(costs[j])
 
-    t_bajari_bids.append(np.array(t_bids))
+    t_bajari_exps.append(np.array(t_exps))
     t_bajari_costs.append(np.array(t_costs))
 
 # plots
+# 1. equilibrium bids
 plt.figure()
 styles = ['-r', '--b']
 style = its.cycle(styles)
+legend = ['Bidder ' + str(i) for i in range(n)]
 for i in range(n):
     plt.plot(dm_costs[i], dm_bids, next(style))
 plt.grid()
-plt.legend(['Bidder ' + str(i) for i in range(n)], loc='upper left')
+plt.legend(legend, loc='upper left')
 plt.savefig('dm.pdf')
 
 plt.figure()
 for i in range(n):
     plt.plot(bajari_costs[i], bajari_bids, next(style))
 plt.grid()
-plt.legend(['Bidder ' + str(i) for i in range(n)], loc='upper left')
+plt.legend(legend, loc='upper left')
 plt.savefig('bajari.pdf')
 
+# 2. expected utilities
+plt.figure()
+for i in range(n):
+    plt.plot(dm_costs[i], dm_exp_utilities[i], next(style))
+plt.grid()
+plt.legend(legend)
+plt.savefig('dm_exp_utilities.pdf')
+
+plt.figure()
+for i in range(n):
+    plt.plot(bajari_costs[i], bajari_exp_utilities[i], next(style))
+plt.grid()
+plt.legend(legend)
+plt.savefig('bajari_exp_utilities.pdf')
+
+# 3. expected utilities for corresponding bidders across two auctions
 for i in range(n):
     plt.figure()
-    plt.plot(dm_costs[i], dm_bids, '-r')
-    plt.plot(t_bajari_costs[i], t_bajari_bids[i], '--b')
+    plt.plot(dm_costs[i], dm_exp_utilities[i], '-r')
+    plt.plot(t_bajari_costs[i], t_bajari_exps[i], '--b')
     plt.grid()
-    plt.legend(list(map(lambda x: x + str(i), ['DM Bidder ', 'Bajari Bidder '])), loc='upper left')
+    plt.legend(list(map(lambda x: x + str(i), ['DM Bidder ', 'Bajari Bidder '])), loc='upper right')
     plt.savefig('compare_' + str(i) + '.pdf')
