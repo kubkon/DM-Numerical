f43d7af7312789bdcfa1824ae3ec87ac1c52ec0c
Jakub Konka <kubkon@gmail.com>
2014-01-10 10:15:48 +0000

    Update bajari_sufficiency script to permit analysis of PPM output.

--- equilibrium/cython/bajari_sufficiency.py
+++ equilibrium/cython/bajari_sufficiency.py
@@ -2,6 +2,7 @@ import argparse
 import ast
 import csv
 from itertools import cycle, chain
+from functools import partial
 
 import numpy as np
 import matplotlib
@@ -37,16 +38,36 @@ with open(file_name, 'rt') as f:
 support = ast.literal_eval(data_in['support'])
 params = ast.literal_eval(data_in['params'])
 n = len(params)
-costs = np.array([ast.literal_eval(data_in['costs_{}'.format(i)]) for i in range(n)])
-bids = np.array(ast.literal_eval(data_in['bids']))
+
+try:
+    costs = np.array([ast.literal_eval(data_in['costs_{}'.format(i)]) for i in range(n)])
+    bids = np.array(ast.literal_eval(data_in['bids']))
+
+except KeyError:
+  bs = [ast.literal_eval(data_in['b_lower']), support[1]]
+  css = [ast.literal_eval(data_in['cs_{}'.format(i)]) for i in range(n)]
 
 # Verify sufficiency
 cdfs = []
 for p in params:
-  cdfs.append(skewnormal(p['shape'], loc=p['location'], scale=p['scale']))
+    cdfs.append(skewnormal(p['shape'], loc=p['location'], scale=p['scale']))
 
-step = len(bids) // 35
-s_costs, s_bids = verify_sufficiency(costs, bids, support[1], cdfs, step=step)
+try:
+    step = len(bids) // 35
+    s_costs, s_bids = verify_sufficiency(costs, bids, support[1], cdfs, step=step)
+
+except NameError:
+    # Define inverse bid function
+    def cost_func(cs, x):
+        return bs[0] + sum([c*(x-bs[0])**i for c,i in zip(cs, range(len(cs)))])
+
+    # Compute bids and costs
+    bids = np.linspace(bs[0], bs[1], 10000)
+    cost_funcs = [partial(cost_func, cs) for cs in css]
+    costs = np.array([[f(b) for b in bids] for f in cost_funcs])
+
+    step = len(bids) // 50
+    s_costs, s_bids = verify_sufficiency(costs, bids, support[1], cdfs, step=step)
 
 # Plot
 styles = ['b', 'r--', 'g:', 'm-.']
@@ -55,7 +76,7 @@ colors = ['b.', 'r.', 'g.', 'm.']
 plt.figure()
 sts = cycle(styles)
 for c in costs:
-  plt.plot(c, bids, next(sts))
+    plt.plot(c, bids, next(sts))
 plt.grid()
 # plt.xlim(support)
 # plt.ylim([int(bids[0]), bids[-1]])
@@ -69,8 +90,8 @@ plt.figure()
 sts = cycle(styles)
 clss = cycle(colors)
 for c, sc, sb in zip(costs, s_costs, s_bids):
-  plt.plot(c, bids, next(sts))
-  plt.plot(sc, sb, next(clss))
+    plt.plot(c, bids, next(sts))
+    plt.plot(sc, sb, next(clss))
 plt.grid()
 plt.xlim(support)
 plt.ylim([int(bids[0]), bids[-1]])
@@ -83,12 +104,12 @@ plt.legend(labels, loc='upper left')
 plt.savefig('sufficiency.pdf')
 
 for i, c, sc, sb in zip(range(1, n+1), costs, s_costs, s_bids):
-  plt.figure()
-  plt.plot(c, bids, 'b')
-  plt.plot(sc, sb, 'r.')
-  plt.grid()
-  plt.xlim(support)
-  plt.ylim([int(bids[0]), bids[-1]])
-  plt.xlabel(r"Cost, $c_i$")
-  plt.ylabel(r"Bid, $b_i$")
-  plt.savefig('sufficiency_{}.pdf'.format(i))
+    plt.figure()
+    plt.plot(c, bids, 'b')
+    plt.plot(sc, sb, 'r.')
+    plt.grid()
+    plt.xlim(support)
+    plt.ylim([int(bids[0]), bids[-1]])
+    plt.xlabel(r"Cost, $c_i$")
+    plt.ylabel(r"Bid, $b_i$")
+    plt.savefig('sufficiency_{}.pdf'.format(i))
