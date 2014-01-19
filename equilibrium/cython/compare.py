bf79c657522580c7e48e925958b485bb55dfc3ca
Jakub Konka <kubkon@gmail.com>
2014-01-19 16:26:52 +0000

    Parallelise comparison script.

--- equilibrium/cython/compare.py
+++ equilibrium/cython/compare.py
@@ -1,8 +1,6 @@
+import argparse
 import itertools as its
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib import rc
 import scipy.stats as ss
 
 import bajari.fsm.main as bajari
@@ -10,82 +8,63 @@ import dm.fsm.main as dm
 from bajari.dists.main import skewnormal
 from util.util import compute_expected_utilities, ks_statistic, polyfit
 
-rc('font',**{'family':'sans-serif','sans-serif':['Gill Sans']})
-rc('text', usetex=True)
-matplotlib.rcParams.update({'font.size': 14, 'legend.fontsize': 14})
 
+# parse command line arguments
+parser = argparse.ArgumentParser(description="Compare auction models")
+parser.add_argument('--w', type=float, help='price weight')
+parser.add_argument('--reps', action='append', type=float, help='reputation array')
+parser.add_argument('--locs', action='append', type=float, help='distributions locations')
+parser.add_argument('--scales', action='append', type=float, help='distributions scales')
+parser.add_argument('--shapes', action='append', type=float, help='distributions shapes')
+args = parser.parse_args()
+w = args.w
+reputations = np.array(args.reps)
+locations = np.array(args.locs)
+scales = np.array(args.scales)
+shapes = np.array(args.shapes)
 
-def compare(w, reputations, locations, scales, shapes):
-    # estimate lower and upper extremities
-    n = reputations.size
-    lowers = np.empty(n, dtype=np.float)
-    uppers = np.empty(n, dtype=np.float)
-    for i in np.arange(n):
-        lowers[i] = (1-w) * reputations[i]
-        uppers[i] = (1-w) * reputations[i] + w
+# estimate lower and upper extremities
+n = reputations.size
+lowers = np.empty(n, dtype=np.float)
+uppers = np.empty(n, dtype=np.float)
+for i in np.arange(n):
+    lowers[i] = (1-w) * reputations[i]
+    uppers[i] = (1-w) * reputations[i] + w
 
-    # approximate the scenario as common support, differing
-    # normal distributions
-    support = [lowers[0], uppers[1]]
-    bajari_params = [{'location': loc, 'scale': scale, 'shape': shape} for loc, scale, shape in zip(locations, scales, shapes)]
+# approximate the scenario as common support, differing
+# normal distributions
+support = [lowers[0], uppers[1]]
+bajari_params = [{'location': loc, 'scale': scale, 'shape': shape} for loc, scale, shape in zip(locations, scales, shapes)]
 
-    # compute approximations
-    dm_bids, dm_costs = dm.solve(w, reputations)
-    bajari_bids, bajari_costs = bajari.solve(support, bajari_params)
+# compute approximations
+dm_bids, dm_costs = dm.solve(w, reputations)
+bajari_bids, bajari_costs = bajari.solve(support, bajari_params)
 
-    # compute expected utilities for both auctions
-    # 1. DM
-    dm_params = [{'loc': lowers[i], 'scale': w} for i in np.arange(n)]
-    dm_exp_utilities = compute_expected_utilities(dm_bids, dm_costs, ss.uniform, dm_params)
+# compute expected utilities for both auctions
+# 1. DM
+dm_params = [{'loc': lowers[i], 'scale': w} for i in np.arange(n)]
+dm_exp_utilities = compute_expected_utilities(dm_bids, dm_costs, ss.uniform, dm_params)
 
-    # 2. Bajari
-    bajari_exp_utilities = compute_expected_utilities(bajari_bids, bajari_costs, skewnormal, bajari_params)
+# 2. Bajari
+bajari_exp_utilities = compute_expected_utilities(bajari_bids, bajari_costs, skewnormal, bajari_params)
 
-    # fit polynomial curve to expected utility functions and
-    # compute KS statistic (distortion between the expected utilities)
-    dm_exp_funcs = []
-    bajari_exp_funcs = []
-    ks_values = []
-
-    for i in np.arange(n):
-        # fit
-        dm_exp_func = polyfit(dm_costs[i], dm_exp_utilities[i], degree=5)
-        bajari_exp_func = polyfit(bajari_costs[i], bajari_exp_utilities[i], degree=5)
-
-        dm_exp_funcs.append(dm_exp_func)
-        bajari_exp_funcs.append(bajari_exp_func)
-
-        # compute KS statistics
-        costs = np.linspace(dm_costs[i][0], dm_costs[i][-1], 1000)
-        _, ks_value = ks_statistic(costs, bajari_exp_func(costs), dm_exp_func(costs))
-        ks_values.append(ks_value)
-
-    return ks_values
-
-
-# initialize the scenario
-w = 0.5
-reputations = [0.25, 0.75]
-
-lowers = [(1-w) * r for r in reputations]
-uppers = [l + w for l in lowers]
-
-locations = [l + w / 2 for l in lowers]
-scales = np.linspace(w/8, w, 20)
-shapes = [0, 0]
-
+# fit polynomial curve to expected utility functions and
+# compute KS statistic (distortion between the expected utilities)
+dm_exp_funcs = []
+bajari_exp_funcs = []
 ks_values = []
 
-for s in scales:
-    ks = compare(w, np.array(reputations), locations, [s, s], shapes)
-    ks_values.append(ks)
-    
-# plot
-plt.figure()
-ks = list(zip(*ks_values))
-plt.plot(scales, ks[0], '.r')
-plt.plot(scales, ks[1], '.b')
-plt.xlabel(r"Variance, $\rho^2$")
-plt.ylabel(r"KS Statistic")
-plt.grid()
-plt.savefig('compare.pdf')
+for i in np.arange(n):
+    # fit
+    dm_exp_func = polyfit(dm_costs[i], dm_exp_utilities[i], degree=5)
+    bajari_exp_func = polyfit(bajari_costs[i], bajari_exp_utilities[i], degree=5)
+
+    dm_exp_funcs.append(dm_exp_func)
+    bajari_exp_funcs.append(bajari_exp_func)
+
+    # compute KS statistics
+    costs = np.linspace(dm_costs[i][0], dm_costs[i][-1], 1000)
+    _, ks_value = ks_statistic(costs, bajari_exp_func(costs), dm_exp_func(costs))
+    ks_values.append(ks_value)
+
+print(ks_values)
---
+++ equilibrium/cython/run-compare.py
@@ -0,0 +1,91 @@
+import argparse
+import ast
+from itertools import cycle
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess as sub
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description="Compare auction models -- Helper script")
+parser.add_argument('--batch_size', dest='batch_size', default=8,
+                    type=int, help='batch size for multiprocessing')
+args = parser.parse_args()
+batch_size = args.batch_size
+
+# prepare the scenario
+w = 0.5
+reps = [0.25, 0.75]
+locs = [(1-w)*r + w/2 for r in reps]
+shapes = [0, 0]
+scales = np.linspace(w/8, w, 8)
+n = len(reps)
+
+# prepare the subprocess command
+cmd = "python compare.py --w=%f" % w
+
+for r,l,sh in zip(reps, locs, shapes):
+    cmd += " --reps=%f --locs=%f --shapes=%f" % (r,l,sh)
+
+# run comparisons
+try:
+    results = []
+
+    # one process at a time
+    if batch_size == 1:
+        for i,s in zip(range(scales.size), scales):
+            execmd = cmd + ''.join([" --scales=%f" % s for j in range(n)])
+            output = ast.literal_eval(sub.check_output(execmd, shell=True).decode('utf-8').rstrip())
+            results.append((i, output))
+    # in batches
+    else:
+        # split into batches
+        repetitions = scales.size
+        quotient = repetitions // batch_size
+        remainder = repetitions % batch_size
+
+        # run the simulations in parallel as subprocesses
+        num_proc = batch_size if batch_size <= repetitions else remainder
+        
+        procs = []
+        for i in range(num_proc):
+            execmd = cmd + ''.join([" --scales=%f" % scales[i] for j in range(n)])
+            procs.append((i, sub.Popen(execmd, shell=True, stdout=sub.PIPE)))
+
+        while True:
+            for p in procs:
+                output = ast.literal_eval(p[1].communicate()[0].decode('utf-8').rstrip())
+                results.append((p[0], output))
+
+            if len(results) == repetitions:
+                break
+
+            procs = []
+            temp_num = batch_size if num_proc + batch_size <= repetitions else remainder
+
+            for i in range(num_proc, num_proc + temp_num):
+                execmd = cmd + ''.join([" --scales=%f" % scales[i] for j in range(n)])
+                procs.append((i, sub.Popen(execmd, shell=True, stdout=sub.PIPE)))
+            num_proc += temp_num
+
+except OSError as e:
+    print("Execution failed: ", e)
+
+# plot
+plt.figure()
+styles = ['+b', 'xr']
+styles_cycle = cycle(styles)
+
+for i in range(n):
+    xs, ys = [], []
+
+    for r in results:
+        xs.append(scales[r[0]])
+        ys.append(r[1][i])
+
+    plt.plot(xs, ys, next(styles_cycle))
+
+plt.xlabel("Variance")
+plt.ylabel("Kolmogorov-Smirnov statistic")
+plt.grid()
+plt.legend(["Bidder %d" % i for i in range(n)])
+plt.savefig("compare.pdf")
