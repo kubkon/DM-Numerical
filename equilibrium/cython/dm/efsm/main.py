aa9c90a9cdebe8fc4a567c50b804d664e322b219
Jakub Konka <kubkon@gmail.com>
2014-02-09 12:50:45 +0000

    Start reimplmenting extended FSM in Cython directly.
    Function for estimating k(b) and c(b) finished and tested.

--- equilibrium/cython/dm/efsm/main.py
+++ equilibrium/cython/dm/efsm/main.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 from dm.common import upper_bound_bids
-import dm.fsm.fsm_internal as fsm_internal
+import dm.efsm.efsm_internal as efsm_internal
 
 
 def solve(w, reputations, granularity=10000):
@@ -37,7 +37,7 @@ def solve(w, reputations, granularity=10000):
 
         # solve the system
         try:
-            costs = solve_extended(lowers, uppers, bids).T
+            costs = efsm_internal.solve(lowers, uppers, bids).T
 
         except Exception:
             # if an error is raised, set low to guess and continue
@@ -63,71 +63,7 @@ def solve(w, reputations, granularity=10000):
 
     except UnboundLocalError:
         raise Exception("Algorithm failed to converge.")
-
-def solve_extended(lowers, uppers, bids):
-    # estimate k(b) and c(b)
-    k, _ = estimate_kc(bids[0], lowers)
-    n = bids.size
-    m = lowers.size
-
-    if k == m:
-        return fsm_internal.solve(lowers, uppers, bids)
-
-    costs_ = np.empty((n,m), np.float)
-
-    costs = fsm_internal.solve(lowers[:k], uppers[:k], bids)
-    costs = add_extension(m, k, costs, bids)
-    print(costs)
-
-    # find the index of cut off
-    index = np.argmin([np.absolute(c - lowers[k]) for c in costs[:,k]])
-
-    for i in np.arange(index):
-        for j in np.arange(m):
-            costs_[i][j] = costs[i][j]
-
-    costs = fsm_internal.solve(costs[index], uppers, bids[index:])
-
-    for i in np.arange(bids[index:].size):
-        for j in np.arange(m):
-            costs_[index+i][j] = costs[i][j]
-
-    return costs_
-
-def estimate_kc(b_lower, lowers):
-    n = lowers.size
-
-    for k in np.arange(1, n):
-
-        sums = 0
-        for i in np.arange(k+1):
-            sums += 1 / (b_lower - lowers[i])
-
-        c = b_lower - k / sums
-
-        if k < n-1:
-            if lowers[k] <= c and c < lowers[k+1]:
-                break
-
-    return (k+1, c)
-
-def add_extension(n, k, costs, bids):
-    m = bids.size
-    costs_ = np.empty((m, n-k), np.float)
-
-    for i in np.arange(m):
-        b = bids[i]
-
-        sums = 0
-        for j in np.arange(k):
-            sums += 1 / (b - costs[i][j])
-
-        for j in np.arange(n-k):
-            costs_[i][j] = b - (k - 1) / sums
-
-    costs = np.hstack((costs, costs_))
-
-    return costs
+        
 
 if __name__ == "__main__":
     # set the scenario
