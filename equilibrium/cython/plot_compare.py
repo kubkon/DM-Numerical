1e520636b4bdd9d2d71bc827beb3c40eb6244422
Jakub Konka <kubkon@jakubkonka.com>
2014-04-27 13:02:51 +0100

    Add generation of exp utility plots.

--- equilibrium/cython/plot_compare.py
+++ equilibrium/cython/plot_compare.py
@@ -17,7 +17,7 @@ matplotlib.rcParams.update({'font.size': 14, 'legend.fontsize': 14})
 
 
 # set the scenario
-w = 0.75
+w = 0.5
 reputations = np.array([0.25, 0.75], dtype=np.float)
 n = reputations.size
 
@@ -69,9 +69,14 @@ cdfs   = [ss.uniform(**p) for p in params]
 
 dm_utils     = []
 bajari_utils = []
+
+dm_exp_funcs     = []
+bajari_exp_funcs = []
+common_costs     = []
 for i in np.arange(n):
     # common costs
     costs = np.linspace(dm_costs[i][0], min(dm_costs[i][-1], bajari_costs[i][-1]), 1000)
+    common_costs.append(costs)
 
     # derive expected utility functions
     j = (i+1) % 2
@@ -80,10 +85,14 @@ for i in np.arange(n):
         prob = 1 - cdfs[j].cdf(dm_inverses[j](bid))
         return (bid - x) * prob
     
+    dm_exp_funcs.append(dm_exp_util)
+
     def bajari_exp_util(x):
         bid  = bajari_bid_funcs[i](x) 
         prob = 1 - cdfs[j].cdf(bajari_inverses[j](bid))
         return (bid - x) * prob
+
+    bajari_exp_funcs.append(bajari_exp_util)
 
     # compute ex-ante expected utilities
     dm_util     = si.quad(lambda x: dm_exp_util(x) * cdfs[i].pdf(x), costs[0], costs[-1])
@@ -92,7 +101,7 @@ for i in np.arange(n):
     bajari_utils.append(bajari_util[0])
 
 # sample and generate prices for each auction
-size = 10000
+size = 100
 sampled_costs = []
 params = [{'loc': dm_costs[i][0], 'scale': dm_costs[i][-1]-dm_costs[i][0]} for i in np.arange(n)]
 for p in params:
@@ -114,4 +123,21 @@ print(dm_utils)
 print(bajari_utils)
 print(np.mean(dm_prices))
 print(np.mean(bajari_prices))
+
+# plots
+for i in np.arange(n):
+    dm_f     = dm_exp_funcs[i]
+    bajari_f = bajari_exp_funcs[i]
+
+    plt.figure()
+    plt.plot(common_costs[i], [dm_f(x) for x in common_costs[i]], '-r')
+    plt.plot(common_costs[i], [bajari_f(x) for x in common_costs[i]], '--b')
+    xlabel = "Cost, $c_{}$".format(i+1)
+    ylabel = "Expected utility, $\Pi_{}(c_{})$".format(i+1, i+1)
+    plt.xlabel(r"" + xlabel)
+    plt.ylabel(r"" + ylabel)
+    legend = ["DMP Bidder {}".format(i+1), "CP Bidder {}".format(i+1)]
+    plt.legend(legend)
+    plt.grid()
+    plt.savefig('expected_utilities_{}.pdf'.format(i+1))
 
