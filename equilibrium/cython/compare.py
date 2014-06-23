7686baba3fb203952008b0cd01046ca85f15b42f
Jakub Konka <kubkon@jakubkonka.com>
2014-06-23 15:35:32 +0100

    Run comparison for random reputation values.

--- equilibrium/cython/compare.py
+++ equilibrium/cython/compare.py
@@ -54,22 +54,21 @@ bajari_bids, bajari_costs = bajari.solve(support, bajari_params)
 dm_costs, dm_bids = util.ensure_monotonicity(dm_costs, dm_bids)
 bajari_costs, bajari_bids = util.ensure_monotonicity(bajari_costs, bajari_bids)
 
-#------------------------------------------------------
-# precondition solutions: remove endpoints where fails
-# Lipschitz condition
-for c in dm_costs:
-    diffs = list(map(lambda x,y: abs(x-y), c, dm_bids))
-    for i in np.arange(len(diffs)):
-        if diffs[i] <= 1e-5:
-            index = i
-            break
+if n == 2:
+    # precondition solutions: remove endpoints where fails
+    # Lipschitz condition
+    for c in dm_costs:
+        diffs = list(map(lambda x,y: abs(x-y), c, dm_bids))
+        for i in np.arange(len(diffs)):
+            if diffs[i] <= 5e-5:
+                index = i
+                break
 
-try:
-    dm_costs = np.array([c[:index] for c in dm_costs])
-    dm_bids  = dm_bids[:index]
-except NameError:
-    pass
-#------------------------------------------------------
+    try:
+        dm_costs = np.array([c[:index] for c in dm_costs])
+        dm_bids  = dm_bids[:index]
+    except NameError:
+        pass
 
 # interpolate bidding functions and their inverses
 dm_bid_funcs     = []
@@ -121,7 +120,7 @@ for i in np.arange(n):
     bajari_utils.append(bajari_util)
 
 # sample and generate prices for each auction
-size = 10000
+size = 10
 sampled_costs = []
 params = [{'loc': common_costs[i][0], 'scale': common_costs[i][-1]-common_costs[i][0]} for i in np.arange(n)]
 for p in params:
--- equilibrium/cython/run_compare.py
+++ equilibrium/cython/run_compare.py
@@ -3,6 +3,7 @@ import ast
 import csv
 from itertools import cycle, repeat
 import numpy as np
+from numpy.random import choice
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc
@@ -14,76 +15,83 @@ matplotlib.rcParams.update({'font.size': 14, 'legend.fontsize': 14})
 
 # parse command line arguments
 parser = argparse.ArgumentParser(description="Compare auction models -- helper script")
-parser.add_argument('reps', nargs='+', type=float, help='reputation array')
+parser.add_argument('n_bidders', type=int, help='number of bidders')
+parser.add_argument('n_ws', type=int, help='number of price weight values')
+parser.add_argument('n_reps', type=int, help='number of reputation values to draw per price weight')
 parser.add_argument('--batch_size', dest='batch_size', default=8,
                     type=int, help='batch size for multiprocessing')
 args = parser.parse_args()
-reps = args.reps
+n_bidders = args.n_bidders
+n_ws = args.n_ws
+n_reps = args.n_reps
 batch_size = args.batch_size
 
 # prepare the scenario
-ws = np.linspace(0.55, 0.99, 40)
-n = len(reps)
+ws = np.linspace(0.55, 0.99, n_ws)
+rands = choice(np.linspace(0.01, 0.99, 10000), n_bidders*n_reps, replace=False)
+reputations = [sorted(rands[i:i+n_bidders].tolist()) for i in range(0, len(rands), n_bidders)]
 
-# prepare the subprocess commands
-cmds = []
-
+results = {}
 for w in ws:
-    cmd =  "python compare.py --w=%r " % w
-    cmd += " ".join(["--reps=%r" % r for r in reps])
-    cmds.append(cmd)
+    # prepare the subprocess commands
+    cmds = []
 
-# run comparisons
-try:
-    results = []
+    for reps in reputations:
+        cmd =  "python compare.py --w=%r " % w
+        cmd += " ".join(["--reps=%r" % r for r in reps])
+        cmds.append(cmd)
 
-    # one process at a time
-    if batch_size == 1:
-        for cmd in cmds:
-            output = ast.literal_eval(sub.check_output(cmd, shell=True)
-                        .decode('utf-8').rstrip())
-            results.append(output)
-    # in batches
-    else:
-        # split into batches
-        repetitions = len(cmds)
-        quotient = repetitions // batch_size
-        remainder = repetitions % batch_size
-
-        # run the simulations in parallel as subprocesses
-        num_proc = batch_size if batch_size <= repetitions else remainder
-        
-        procs = []
-        for i in range(num_proc):
-            procs.append(sub.Popen(cmds[i], shell=True, stdout=sub.PIPE))
-
-        while True:
-            for p in procs:
-                output = ast.literal_eval(p.communicate()[0]
+    # run comparisons
+    try:
+        # one process at a time
+        if batch_size == 1:
+            for cmd in cmds:
+                output = ast.literal_eval(sub.check_output(cmd, shell=True)
                             .decode('utf-8').rstrip())
-                results.append(output)
+                results.setdefault(w, []).append(output)
+        # in batches
+        else:
+            # split into batches
+            repetitions = len(cmds)
+            quotient = repetitions // batch_size
+            remainder = repetitions % batch_size
 
-            if len(results) == repetitions:
-                break
-
+            # run the simulations in parallel as subprocesses
+            num_proc = batch_size if batch_size <= repetitions else remainder
+            
             procs = []
-            temp_num = batch_size if num_proc + batch_size <= repetitions else remainder
-
-            for i in range(num_proc, num_proc + temp_num):
+            for i in range(num_proc):
                 procs.append(sub.Popen(cmds[i], shell=True, stdout=sub.PIPE))
-            num_proc += temp_num
 
-except OSError as e:
-    print("Execution failed: ", e)
+            while True:
+                for p in procs:
+                    output = ast.literal_eval(p.communicate()[0]
+                                .decode('utf-8').rstrip())
+                    results.setdefault(w, []).append(output)
 
-# save results to files
-prefix   = '_'.join(list(map(lambda x: str(int(x*100)), reps)))
-for t in ['dm', 'cp']:
-    with open(prefix + "_compare_%s.csv" % t, "wt") as f:
-        writer = csv.writer(f)
-        writer.writerow(["w", "price"] + ["bidder_{}".format(i+1) for i in range(n)])
+                if len(results[w]) == repetitions:
+                    break
+
+                procs = []
+                temp_num = batch_size if num_proc + batch_size <= repetitions else remainder
+
+                for i in range(num_proc, num_proc + temp_num):
+                    procs.append(sub.Popen(cmds[i], shell=True, stdout=sub.PIPE))
+                num_proc += temp_num
+
+    except OSError as e:
+        print("Execution failed: ", e)
+
+print(results)
+
+# # save results to files
+# prefix   = '_'.join(list(map(lambda x: str(int(x*100)), reps)))
+# for t in ['dm', 'cp']:
+#     with open(prefix + "_compare_%s.csv" % t, "wt") as f:
+#         writer = csv.writer(f)
+#         writer.writerow(["w", "price"] + ["bidder_{}".format(i+1) for i in range(n)])
     
-        for w,r in zip(ws, results):
-            values = [w, r['prices'][t]] + r['utilities'][t]
-            writer.writerow(list(map(lambda x: '%r' % x, values)))
+#         for w,r in zip(ws, results):
+#             values = [w, r['prices'][t]] + r['utilities'][t]
+#             writer.writerow(list(map(lambda x: '%r' % x, values)))
 
