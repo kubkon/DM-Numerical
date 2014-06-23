3f740725524d2426992a3b741302af666e2ad8d4
Jakub Konka <kubkon@jakubkonka.com>
2014-06-23 16:39:05 +0100

    Finally, write results to file.

--- equilibrium/cython/run_compare.py
+++ equilibrium/cython/run_compare.py
@@ -84,42 +84,38 @@ for w in ws:
         print("Execution failed: ", e)
 
 # calculate averages and confidence intervals
-print(results)
+with open(str(n_bidders) + '_compare.csv', 'wt') as f:
+    writer = csv.writer(f)
+    headers = ['w', 'price mean', 'price ci']
+    for b in ['bidder_{}'.format(i+1) for i in range(n_bidders)]:
+        headers.extend([b + ' mean', b + ' ci'])
+    writer.writerow(headers)
 
-for w in ws:
-    prices_errors = []
-    utilities_errors = []
-    for r in results[w]:
-        prices = r['prices']
-        prices_errors.append(abs((prices['dm'] - prices['cp']) / prices['dm']) * 100)
+    for w in ws:
+        prices_errors = []
+        utilities_errors = []
+        for r in results[w]:
+            prices = r['prices']
+            prices_errors.append(abs((prices['dm'] - prices['cp']) / prices['dm']) * 100)
 
-        utilities = r['utilities']
-        error = list(map(lambda x,y: abs((x - y) / x) * 100, utilities['dm'], utilities['cp']))
-        utilities_errors.append(error)
+            utilities = r['utilities']
+            error = list(map(lambda x,y: abs((x - y) / x) * 100, utilities['dm'], utilities['cp']))
+            utilities_errors.append(error)
 
-    prices_mean = np.mean(prices_errors)
-    prices_std  = np.sqrt(np.var(prices_errors))
-    alpha       = 0.05
-    n           = len(prices_errors)
-    prices_ci   = t.ppf(1 - alpha/2, n-1) * prices_std / np.sqrt(n)
+        prices_mean = np.mean(prices_errors)
+        prices_std  = np.sqrt(np.var(prices_errors))
+        alpha       = 0.05
+        n           = len(prices_errors)
+        prices_ci   = t.ppf(1 - alpha/2, n-1) * prices_std / np.sqrt(n)
 
-    utilities_means = []
-    utilities_cis = []
-    for x_y in zip(*utilities_errors):
-        mean = np.mean(x_y)
-        std  = np.sqrt(np.var(x_y))
-        ci   = t.ppf(1 - alpha/2, n-1) * std / np.sqrt(n)
-        utilities_means.append(mean)
-        utilities_cis.append(ci)
+        utilities = []
+        for x_y in zip(*utilities_errors):
+            mean = np.mean(x_y)
+            std  = np.sqrt(np.var(x_y))
+            ci   = t.ppf(1 - alpha/2, n-1) * std / np.sqrt(n)
+            utilities.extend([mean, ci])
 
-# # save results to files
-# prefix   = '_'.join(list(map(lambda x: str(int(x*100)), reps)))
-# for t in ['dm', 'cp']:
-#     with open(prefix + "_compare_%s.csv" % t, "wt") as f:
-#         writer = csv.writer(f)
-#         writer.writerow(["w", "price"] + ["bidder_{}".format(i+1) for i in range(n)])
-    
-#         for w,r in zip(ws, results):
-#             values = [w, r['prices'][t]] + r['utilities'][t]
-#             writer.writerow(list(map(lambda x: '%r' % x, values)))
+        # save results to files
+        values = [w, prices_mean, prices_ci] + utilities 
+        writer.writerow(list(map(lambda x: '%r' % x, values)))
 
